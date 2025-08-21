0d45a611d6d03851c477ca39f7a31e4313d3f2ca
kosilov Ilya <godzilloff@gmail.com>
2025-08-21 10:22:37 +0300

    hw7: clean(tests): убрал лишний мусор

--- hw7/parser/test/parser_tests.cpp
+++ hw7/parser/test/parser_tests.cpp
@@ -25,7 +25,7 @@ protected:
         
         observable = std::make_shared<Observable>();
         observable->subscribe(std::make_shared<ConsoleObserver>());
-        observable->subscribe(std::make_shared<FileObserver>());
+        //observable->subscribe(std::make_shared<FileObserver>());
         
         processor->set_observable(observable);
     }
@@ -62,98 +62,3 @@ TEST_F(ParseFixture, Nested) {
     processor->process_command("}");
     EXPECT_EQ( GetCapturedOutput(), std::string("bulk: cmd1\nbulk: cmd21, cmd22\n") );
 }
-
-// const size_t CNT = 10;
-
-// TEST(Parser, Empty) {
-//     // Arrange
-//     // Matrix<int,0> matrix_empty;
-
-//     // Act (empty for this test)
-
-//     // Assert
-//     ASSERT_EQ(0, static_cast<size_t>(0));
-//     ASSERT_TRUE(true);
-// }
-
-// struct MatrixFixture : public testing::Test {
-//     const size_t cnt = CNT;
-//     Matrix<int,0> matrix;
-
-//     // Per-test-suite set-up.
-//     static void SetUpTestSuite() {
-//         // std::cout << "SetUpTestSuite" << std::endl;
-//     }
-
-//     // Per-test-suite tear-down.
-//     static void TearDownTestSuite() {
-//         // std::cout << "TearDownTestSuite" << std::endl;
-//     }
-
-//     // Per-test set-up
-//     void SetUp() override {
-//         std::cout << "SetUp" << std::endl;
-
-//         // Create list with elementCount elements    // Заполнение главной диагонали
-//         for (size_t i = 0; i < CNT; ++i) {
-//             matrix[i][i] = i;
-//         }
-
-//         // Заполнение второстепенной диагонали
-//         for (size_t i = 0; i < CNT; ++i) {
-//             matrix[i][9 - i] = 9 - i;
-//         }
-
-//     }
-
-//     // You can define per-test tear-down logic as usual.
-//     void TearDown() override {
-//         // std::cout << "TearDown" << std::endl;
-//     }
-// };
-
-
-// TEST(Matrix, Empty) {
-//     // Arrange
-//     Matrix<int,0> matrix_empty;
-
-//     // Act (empty for this test)
-
-//     // Assert
-//     ASSERT_EQ(matrix_empty.size(), static_cast<size_t>(0));
-//     ASSERT_TRUE(matrix_empty.empty());
-// }
-
-// TEST(Matrix, PushBack) {
-//     // Arrange
-//     const size_t count = CNT;
-//     Matrix<int, 0> matrix;
-
-//     // Act
-//     for (size_t i = 0; i < count; ++i)
-//         matrix[i][i] = i;
-
-//     // Заполнение второстепенной диагонали
-//     for (size_t i = 0; i < count; ++i)
-//         matrix[i][9 - i] = 9 - i;
-
-//     // Assert
-//     ASSERT_FALSE(matrix.empty());
-//     ASSERT_EQ(matrix.size(), static_cast<size_t>(18));
-// }
-
-
-// TEST_F(MatrixFixture, Operator) {
-//     // Arrange
-//     // from fixture
-
-//     // Act    
-//     ((matrix[100][100] = 314) = 0) = 217;
-//     std::cout << "matrix[100][100] = " << matrix[100][100] << std::endl;
-
-//     // Assert
-//     ASSERT_EQ(matrix.size(), static_cast<size_t>(19));
-//     ASSERT_FALSE(matrix.empty());
-
-//     ASSERT_EQ(matrix[100][100], static_cast<size_t>(217));
-// }
